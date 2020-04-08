b9f778dd0874b64df93bd34a02774f058dcd8785
Chetan Chauhan <chauhanchetan82@gmail.com>
2020-04-08 23:16:36 +0530

    Projects and Blog section added

---
+++ projects/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from projects.models import Project
+
+# for index page listing all the projects
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+# for project details page with details of a single project
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
+
